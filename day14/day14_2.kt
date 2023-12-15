1121791a83a6dbb7a6032278c0dcd494b0c0aa94
David Möller <damoller99@hotmail.se>
2023-12-15 15:16:31 +0100

    Optimising day 14

--- day14/day14_2.kt
+++ day14/day14_2.kt
@@ -1,92 +1,66 @@
 import java.io.File
 import java.util.TreeSet
 
-fun roll_north(bolders: MutableList<Pair<Int, Int>>, cubes_y: List<TreeSet<Int>>) {
+fun roll_north(bolders: MutableList<Pair<Int, Int>>, destination_grid: List<List<Pair<Int, Int>>>) {
     var counts = HashMap<Pair<Int, Int>, Int>()
     for (i in 0 ..< bolders.size) {
-        var closest = cubes_y[bolders[i].first].lower(bolders[i].second)
-        var new_pos: Pair<Int, Int>
-        if (closest === null) {
-            new_pos = Pair(bolders[i].first, 0)
-        } else {
-            new_pos = Pair(bolders[i].first, closest + 1)
-        }
-        var corrected_pos = Pair(new_pos.first, new_pos.second + counts.getOrDefault(new_pos, 0))
+        var next = destination_grid[bolders[i].second][bolders[i].first]
+        var corrected_pos = Pair(next.first, next.second + counts.getOrDefault(next, 0))
         bolders.set(i, corrected_pos)
-        counts.set(new_pos, counts.getOrDefault(new_pos, 0) + 1)
+        counts.set(next, counts.getOrDefault(next, 0) + 1)
     }
 }
 
 fun roll_south(
         bolders: MutableList<Pair<Int, Int>>,
-        cubes_y: List<TreeSet<Int>>,
-        max_y: Int,
+        destination_grid: List<List<Pair<Int, Int>>>,
 ) {
     var counts = HashMap<Pair<Int, Int>, Int>()
     for (i in 0 ..< bolders.size) {
-        var closest = cubes_y[bolders[i].first].higher(bolders[i].second)
-        var new_pos: Pair<Int, Int>
-        if (closest === null) {
-            new_pos = Pair(bolders[i].first, max_y)
-        } else {
-            new_pos = Pair(bolders[i].first, closest - 1)
-        }
-        var corrected_pos = Pair(new_pos.first, new_pos.second - counts.getOrDefault(new_pos, 0))
+        var next = destination_grid[bolders[i].second][bolders[i].first]
+        var corrected_pos = Pair(next.first, next.second - counts.getOrDefault(next, 0))
         bolders.set(i, corrected_pos)
-        counts.set(new_pos, counts.getOrDefault(new_pos, 0) + 1)
+        counts.set(next, counts.getOrDefault(next, 0) + 1)
     }
 }
 
 fun roll_west(
         bolders: MutableList<Pair<Int, Int>>,
-        cubes_x: List<TreeSet<Int>>,
+        destination_grid: List<List<Pair<Int, Int>>>,
 ) {
     var counts = HashMap<Pair<Int, Int>, Int>()
     for (i in 0 ..< bolders.size) {
-        var closest = cubes_x[bolders[i].second].lower(bolders[i].first)
-        var new_pos: Pair<Int, Int>
-        if (closest === null) {
-            new_pos = Pair(0, bolders[i].second)
-        } else {
-            new_pos = Pair(closest + 1, bolders[i].second)
-        }
-        var corrected_pos = Pair(new_pos.first + counts.getOrDefault(new_pos, 0), new_pos.second)
+        var next = destination_grid[bolders[i].second][bolders[i].first]
+        var corrected_pos = Pair(next.first + counts.getOrDefault(next, 0), next.second)
         bolders.set(i, corrected_pos)
-        counts.set(new_pos, counts.getOrDefault(new_pos, 0) + 1)
+        counts.set(next, counts.getOrDefault(next, 0) + 1)
     }
 }
 
 fun roll_east(
         bolders: MutableList<Pair<Int, Int>>,
-        cubes_x: List<TreeSet<Int>>,
-        max_x: Int,
+        destination_grid: List<List<Pair<Int, Int>>>,
 ) {
     var counts = HashMap<Pair<Int, Int>, Int>()
     for (i in 0 ..< bolders.size) {
-        var closest = cubes_x[bolders[i].second].higher(bolders[i].first)
-        var new_pos: Pair<Int, Int>
-        if (closest === null) {
-            new_pos = Pair(max_x, bolders[i].second)
-        } else {
-            new_pos = Pair(closest - 1, bolders[i].second)
-        }
-        var corrected_pos = Pair(new_pos.first - counts.getOrDefault(new_pos, 0), new_pos.second)
+        var next = destination_grid[bolders[i].second][bolders[i].first]
+        var corrected_pos = Pair(next.first - counts.getOrDefault(next, 0), next.second)
         bolders.set(i, corrected_pos)
-        counts.set(new_pos, counts.getOrDefault(new_pos, 0) + 1)
+        counts.set(next, counts.getOrDefault(next, 0) + 1)
     }
 }
 
 fun cycle(
         bolders: MutableList<Pair<Int, Int>>,
-        cubes_x: List<TreeSet<Int>>,
-        cubes_y: List<TreeSet<Int>>,
-        max_x: Int,
-        max_y: Int
+        destination_grid_north: List<List<Pair<Int, Int>>>,
+        destination_grid_south: List<List<Pair<Int, Int>>>,
+        destination_grid_west: List<List<Pair<Int, Int>>>,
+        destination_grid_east: List<List<Pair<Int, Int>>>,
 ) {
-    roll_north(bolders, cubes_y)
-    roll_west(bolders, cubes_x)
-    roll_south(bolders, cubes_y, max_y)
-    roll_east(bolders, cubes_x, max_x)
+    roll_north(bolders, destination_grid_north)
+    roll_west(bolders, destination_grid_west)
+    roll_south(bolders, destination_grid_south)
+    roll_east(bolders, destination_grid_east)
 }
 
 fun print_rocks(
@@ -131,12 +105,81 @@ fun main() {
         }
     }
 
-    for (i in 1..1000_000_000) {
-        if (i % 10_000 == 0) {
+    var destination_grid_north =
+            MutableList<MutableList<Pair<Int, Int>>>(lines.size) {
+                MutableList(lines[it].length) { Pair(0, 0) }
+            }
+
+    for (y in 0 ..< lines.size) {
+        for (x in 0 ..< lines[y].length) {
+            var closest = cubes_y[x].lower(y)
+            var new_pos: Pair<Int, Int> = Pair(x, 0)
+            if (closest !== null) {
+                new_pos = Pair(x, closest + 1)
+            }
+            destination_grid_north[y][x] = new_pos
+        }
+    }
+
+    var destination_grid_south =
+            MutableList<MutableList<Pair<Int, Int>>>(lines.size) {
+                MutableList(lines[it].length) { Pair(0, 0) }
+            }
+
+    for (y in 0 ..< lines.size) {
+        for (x in 0 ..< lines[y].length) {
+            var closest = cubes_y[x].higher(y)
+            var new_pos: Pair<Int, Int> = Pair(x, lines.size - 1)
+            if (closest !== null) {
+                new_pos = Pair(x, closest - 1)
+            }
+            destination_grid_south[y][x] = new_pos
+        }
+    }
+
+    var destination_grid_west =
+            MutableList<MutableList<Pair<Int, Int>>>(lines.size) {
+                MutableList(lines[it].length) { Pair(0, 0) }
+            }
+
+    for (y in 0 ..< lines.size) {
+        for (x in 0 ..< lines[y].length) {
+            var closest = cubes_x[y].lower(x)
+            var new_pos: Pair<Int, Int> = Pair(0, y)
+            if (closest !== null) {
+                new_pos = Pair(closest + 1, y)
+            }
+            destination_grid_west[y][x] = new_pos
+        }
+    }
+
+    var destination_grid_east =
+            MutableList<MutableList<Pair<Int, Int>>>(lines.size) {
+                MutableList(lines[it].length) { Pair(0, 0) }
+            }
+
+    for (y in 0 ..< lines.size) {
+        for (x in 0 ..< lines[y].length) {
+            var closest = cubes_x[y].higher(x)
+            var new_pos: Pair<Int, Int> = Pair(lines[0].length - 1, y)
+            if (closest !== null) {
+                new_pos = Pair(closest - 1, y)
+            }
+            destination_grid_east[y][x] = new_pos
+        }
+    }
+
+    for (i in 1..1_000_000_000) {
+        if (i % 1_000 == 0) {
             println(i)
         }
-        cycle(positions, cubes_x, cubes_y, lines[0].length - 1, lines.size - 1)
+        cycle(
+                positions,
+                destination_grid_north,
+                destination_grid_south,
+                destination_grid_west,
+                destination_grid_east
+        )
     }
-    // cycle(positions, cubes_x, cubes_y, lines[0].length - 1, lines.size - 1)
-    print_rocks(positions.toSet(), setOf(), lines[0].length, lines.size, lines)
+    println(positions.fold(0) { a, b -> a + lines.size - b.second })
 }
